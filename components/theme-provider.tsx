0fd3242d1fd5dcb1dc3f18158ad5acd7313bc633
Rashsea <kaji.dev3927@gmail.com>
2025-10-31 18:08:04 +0900

    feat: ThemeProviderコンポーネントを追加

---
+++ components/theme-provider.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { useEffect, useState } from 'react';
+
+export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+
+  return mounted && <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
