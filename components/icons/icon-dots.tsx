d698e4e0a9bf6793ff88985738722c554d7adbcc
Rashsea <kaji.dev3927@gmail.com>
2025-11-05 18:15:11 +0900

    feat: アイコンコンポーネントを追加
    - IconDotsコンポーネントを追加
    - IconSortコンポーネントを追加

---
+++ components/icons/icon-dots.tsx
@@ -0,0 +1,15 @@
+const IconDots = () => {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='none' viewBox='0 0 20 20'>
+      <path
+        className='stroke-neutral-900 dark:stroke-neutral-0'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='1.6'
+        d='M10 10.833a.833.833 0 1 0 0-1.666.833.833 0 0 0 0 1.666M10 5a.833.833 0 1 0 0-1.667A.833.833 0 0 0 10 5M10 16.667A.833.833 0 1 0 10 15a.833.833 0 0 0 0 1.667'
+      />
+    </svg>
+  );
+};
+
+export default IconDots;
